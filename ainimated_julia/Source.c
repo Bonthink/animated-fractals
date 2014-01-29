7f33eed7f6008b637e5768b58621a6a762849643
Bonthink <aburov.post@gmail.com>
2014-01-29 21:15:02 +0400

    Final animated Julia Set
    For interaction use
    F1 / F2 scale up/down
    F3        enable/disable animation
    F4 / F5 speed up/down animation
    Up/Down/Left/Right for moving

--- ainimated_julia/Source.c
+++ ainimated_julia/Source.c
@@ -1,55 +1,77 @@
 #include <stdlib.h>
 #include <glut.h>
+#include <string.h>
 
-int win_width = 800;
-int win_height = 640;
-float cRe = -1.2;
+// Initial window sizes
+int win_width = 900;
+int win_height = 600;
+// Initial c values
+float cRe = - 1.9;
 float cIm = 0.245f;
-int flag = 0;
-int control_timer = 0; // Indicates is the program interactive
-int control_anim = 0;
+int cReIsShrinking = 0;
+// Show if animation or move timers work
+int control_anim = 0; 
+int control_move = 0;
+// Show if parameters are changing
+float delta_c = 0.01;
 int delta_scale = 0;
 int delta_move_x = 0;
 int delta_move_y = 0;
-float scale = 1;
+// Parametrs
+float scale = 0.3;
 float move_x = 0;
 float move_y = 0;
 
-void timer(void)
-{
-	if (flag == 0)
-		cRe += 0.01;
-	else
-		cRe -= 0.01;
-
-	if (cRe > 1)
-		flag = 1;
-	if (cRe < -2)
-		flag = 0;
-
-	glutPostRedisplay();
-	if (control_timer == 0)
-	glutTimerFunc(60, timer, 0);
-}
+// Produces animation
 
 void anim_timer(void)
 {
+	if (cReIsShrinking == 0)
+		cRe += delta_c;
+	else
+		cRe -= delta_c;
+
+	if (cRe > 1)
+		cReIsShrinking = 1;
+	if (cRe < -2)
+		cReIsShrinking = 0;
+
+	glutPostRedisplay();
+	if (control_anim == 0)
+	glutTimerFunc(60, anim_timer, 0);
+}
+
+// Produces moving and scaling
+
+void move_timer(void)
+{
 	glutPostRedisplay();
 
-	if (control_anim == 1)
-	glutTimerFunc(60, anim_timer, 1);
+	if (control_move == 1)
+	glutTimerFunc(60, move_timer, 1);
 }
+
+// Handles keyboard calls
 
 void pressSpecialKey(int key, int x, int y)
 {
 	if (key == GLUT_KEY_F3)
 	{
-		control_timer = (control_timer == 0) ? 1 : 0;
-		if (control_timer == 0)
-			glutTimerFunc(60, timer, 0);
+		control_anim = (control_anim == 0) ? 1 : 0;
+		if (control_anim == 0)
+			glutTimerFunc(60,anim_timer, 0);
 	
 		return;
 	}
+
+	if (key == GLUT_KEY_F4)
+		delta_c -= 0.005;
+
+	if (key == GLUT_KEY_F5)
+		delta_c += 0.005;
+
+	if (control_anim == 0)
+		return;
 
 	switch (key)
 	{
@@ -73,9 +95,11 @@ void pressSpecialKey(int key, int x, int y)
 		break;
 	}
 
-	control_anim = 1;
-	glutTimerFunc(60, anim_timer, 1);
+	control_move = 1;
+	glutTimerFunc(60, move_timer, 1);
 }
+
+// Also handles keyboard calls
 
 void releaseKey(int key, int x, int y)
 {
@@ -83,41 +107,45 @@ void releaseKey(int key, int x, int y)
 	{
 	case GLUT_KEY_F1:
 	case GLUT_KEY_F2:
-		delta_scale = 0.0f;
-		break;
 	case GLUT_KEY_LEFT:
-	case GLUT_KEY_RIGHT: 
-		delta_move_x = 0.0f;
-		break;
+	case GLUT_KEY_RIGHT:
 	case GLUT_KEY_UP:
 	case GLUT_KEY_DOWN: 
+		delta_move_x = 0.0f;
+		delta_scale = 0.0f;
 		delta_move_y = 0.0;
 		break;
 	}
 
-	control_anim = 0;
-	glutPostRedisplay();
+	control_move = 0;
+	glutTimerFunc(60, move_timer, 1);
 }
+
+// Computes new X
 
 float compX(float move)
 {
 	if (delta_move_x > 0)
-		move += 0.001;
+		move += 0.01;
 	else
-		move -= 0.001;
+		move -= 0.01;
 
 	return move;
 }
+
+// Computes new Y
 
 float compY(float move)
 {
 	if (delta_move_y > 0)
-		move += 0.001;
+		move += 0.01;
 	else
-		move -= 0.001;
+		move -= 0.01;
 
 	return move;
 }
+
+// Computes new Scale
 
 float compScale(float scale)
 {
@@ -129,12 +157,16 @@ float compScale(float scale)
 	return scale;
 }
 
+// Drawing function
+
 void display(void)
 {
 	float pixRe, pixIm, zRe, zIm;
 	int x, y, i;
 	int maxIterations = 255;
 	
+	win_width = glutGet(GLUT_WINDOW_WIDTH);
+	win_height = glutGet(GLUT_WINDOW_HEIGHT);
 
 	if (delta_move_x)
 		move_x = compX(move_x);
@@ -144,16 +176,14 @@ void display(void)
 		scale = compScale(scale);
 
 	glClear(GL_COLOR_BUFFER_BIT);
-	glTranslatef(move_x, move_y, 0);
-	glScalef(scale, scale, scale);
-
+	
 	glBegin(GL_POINTS);
 
 	for (int x = 0; x < win_width; x++)
 	for (int y = 0; y < win_height; y++)
 	{
-		pixRe = 1.5 * (x - win_width / 2) / (0.3*win_width);
-		pixIm = (y - win_height / 2) / (0.3 * win_height);
+		pixRe = 1.5 * (x - win_width / 2) / (scale* win_width)+move_x;
+		pixIm = (y - win_height / 2) / (scale * win_height)+move_y;
 
 		glColor3f(0, 0, 0);
 
@@ -173,8 +203,8 @@ void display(void)
 			pixIm = zIm;
 		}
 
-		pixRe = 1.5 * (x - win_width / 2) / (scale * win_width)+move_x;
-		pixIm = (y - win_height / 2) / (scale * win_height)+move_y;
+		pixRe = (x - win_width / 2) / (0.5 * win_width);
+		pixIm = (y - win_height / 2) / (0.5 * win_height);
 		glVertex2f(pixRe, pixIm);
 	}
 
@@ -196,11 +226,10 @@ int main(int argc, char** argv)
 
 	//Register callback functions
 	glutDisplayFunc(display);
-	glutTimerFunc(45, timer, 0);
+	glutTimerFunc(45,anim_timer, 0);
 	glutSpecialFunc(pressSpecialKey);
 	glutIgnoreKeyRepeat(1);
 	glutSpecialUpFunc(releaseKey);
-	//glutIdleFunc(display);
 
 	glutMainLoop();
 
